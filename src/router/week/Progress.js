e338f7c24235e7cc50359296e41c28a02b7a09c0
liugang <liugang@xinhehui.com>
2018-07-12 23:43:34 +0800

    fixedv2.1

--- src/router/week/Progress.js
+++ src/router/week/Progress.js
@@ -12,7 +12,7 @@ export default class Progress extends Component {
 
   increase = () => {
     const percent = this.state.percent;
-    const targetPercent = percent >= 90 ? 100 : percent + 10;
+    const targetPercent = percent >= 90000000000 ? 100 : percent + 10;
     const speed = (targetPercent - percent) / 400;
     let start = null;
     const animate = timestamp => {
@@ -48,16 +48,30 @@ export default class Progress extends Component {
     window.requestAnimationFrame(animate);
   };
 
+  autoAdd = () => {
+    const percent = this.state.percent;
+    const _this = this;
+    // setInterval(function () {
+    //   // conso
+    //   _this.setState({
+    //     percent: percent + 1,
+    //   });
+    // }, 1000);
+    setInterval(() => {
+      this.increase();
+    }, 100)
+  };
+
   render() {
     const { percent } = this.state;
 
     return (
       <div>
         <div className="progress">
-          <div style={{ width: '100 %', height: '100 %', fontSize: 500 }}>{percent}%</div>
+          <div style={{ width: '100 %', height: '100 %', fontSize: 400 }}>{percent}</div>
         </div>
         <div className="btns">
-          <button style={{ width: 300, height: 100, fontSize: 50 }} onClick={this.increase}>addition</button>
+          <button style={{ width: 300, height: 100, fontSize: 50 }} onClick={this.autoAdd}>addition</button>
           <button style={{ width: 300, height: 100, fontSize: 50 }} onClick={this.decrease}>subtraction</button>
         </div>
       </div>
