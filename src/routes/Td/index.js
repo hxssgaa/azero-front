8498056edfde5b070e22c3bc8775f1551837df48
liugang <liugang@xinhehui.com>
2018-09-03 12:56:59 +0800

    修改td数据v1.6

--- src/routes/Td/index.js
+++ src/routes/Td/index.js
@@ -39,13 +39,13 @@ export default class TdForm extends Component {
     console.info(111, data);
     const { status } = data;
     let light;
-    let buttonDetail = 1;
+    let buttonDetail;
     if (parseInt(status, 10) === 0) {
       light = '服务器未开启';
-      buttonDetail = '点击开启同步:';
+      buttonDetail = '当前服务器没有同步数据:';
     } else if (parseInt(status, 10) === 1) {
       light = '服务器已经开启';
-      buttonDetail = '当前数据已经是最新:';
+      buttonDetail = '当前服务器正在同步数据:';
     }
 
     return (
@@ -53,16 +53,14 @@ export default class TdForm extends Component {
         {/* td同步数据开关 */}
         <div className={styles.subProperty}>一.td同步数据开关</div>
         <Row gutter={24}>
-          <Col span={4}>
-            {buttonDetail}
-          </Col>
-          <Col span={12}>
-            <Button
-              type="primary"
+          <Col span={10}>
+            <div
               style={{ marginRight: 20 }}
             >
-              {light}
-            </Button>
+              {buttonDetail + light}
+            </div>
+          </Col>
+          <Col span={12}>
             <Button
               type="primary"
               onClick={this.tdButtonClick.bind(this, '开启')}
