c508d078f8a0806c5e060b1598afb3991c433cb5
liugang <liugang@xinhehui.com>
2018-09-16 22:54:11 +0800

    修改meta维度v1.1

--- src/routes/Ib/index.js
+++ src/routes/Ib/index.js
@@ -1,21 +1,25 @@
 import React, { Component } from 'react';
+import { Button, Select, Row, Col, Progress, Table } from 'antd';
+import rhombus from '../../assets/sync/rhombus.png';
+import rhombusNo from '../../assets/sync/rhombusNo.png';
 import { connect } from 'dva';
+import * as Service from '../../services/api';
 
-@connect(({ Ib, loading }) => ({
-  Ib,
-  loading: loading.effects['Ib/fetch'],
-}))
+// @connect(({ Ib, loading }) => ({
+//   Ib,
+//   loading: loading.effects['Ib/fetch'],
+// }))
 
 export default class IbForm extends Component {
   state = {};
 
-  componentDidMount() {
-    console.info(1111);
-    const { dispatch } = this.props;
-    dispatch({
-      type: 'Ib/fetch',
-    });
-  }
+  // componentDidMount() {
+  //   console.info(1111);
+  //   const { dispatch } = this.props;
+  //   dispatch({
+  //     type: 'Ib/fetch',
+  //   });
+  // }
 
   // componentWillUnmount() {
   //   const { dispatch } = this.props;
@@ -24,10 +28,44 @@ export default class IbForm extends Component {
   //   });
   // }
 
+  laoXiangButtonClick = () => {
+    console.info(4444,'laoxiang');
+    Service.queryTdStartData().then((res) => {
+       console.info(3333,res);
+      });
+  };
+
   render() {
     return (
       <div>
-        2222
+        <div>Td sync data switch</div>
+        <div>
+          <Row gutter={24}>
+            <Col span={10}>
+              <Button
+                type="primary"
+                onClick={this.laoXiangButtonClick.bind(this)}
+                style={{ marginRight: 20, marginBottom: 10 }}
+              >open
+              </Button>
+              <Button
+                type="primary"
+                onClick={() => {
+                  console.info('laoxiangzhendehenhao')
+                }}
+                style={{ marginBottom: 10 }}
+              >close
+              </Button>
+            </Col>
+            <Col span={9} offset={1}>
+              <div>
+                {parseInt(status, 10) === 0 ? 'server is not turned on: ' : 'server is turned on:'}
+                {parseInt(status, 10) === 0 ? <img style={{ width: 16 }} alt={1} src={rhombusNo} /> : <img style={{ width: 16 }} alt={2} src={rhombus} />}
+              </div>
+            </Col>
+
+          </Row>
+        </div>
       </div>
     );
   }
