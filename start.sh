663deffa143be9a5f2254ec9dddd2a431726a841
liugang <liugang@xinhehui.com>
2018-10-05 00:07:05 +0800

    修改ibv1.8

--- start.sh
+++ start.sh
@@ -1,2 +1,2 @@
 git pull
-docker run -it -p 5050:8000 -v /home/hxssg1124/azero-stock-data-sync/:/data/azero-stock-data-sync azero-front bash
+docker run -it -p 5050:8000 -v /home/hxssg1124/azero-front/:/data/azero-front azero-front bash
